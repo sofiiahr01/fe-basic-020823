1d03ef1bc65acacc8d26d648b511e980fe398b42
sofiiahr01 <sgradinarova2001@gmail.com>
2023-10-09 10:18:35 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("It work!");
+
+$(".burger").click(function () {
+  $(this).toggleClass("active");
+  $(".header-nav").toggleClass("active");
+});
+$(".close").click(function () {
+  $(this).toggleClass("active");
+  $(".header-nav").toggleClass("active");
+});
